565ad771eca178863d83cca322c2e691b4a8b91b
apatwa17 <ahpandroid5@gmail.com>
2018-10-13 18:00:29 -0500

    merge conflict resolved?

--- openBorders/src/pages/home/home.ts
+++ openBorders/src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular';
-import { PopoverController } from 'ionic-angular' ;
+import { PopoverController } from 'ionic-angular';
 import { PersonSignupPage } from '../person-signup/person-signup';
 import { PopoverPage } from '../popover/popover';
 
@@ -15,21 +15,17 @@ export class HomePage {
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public popoverCtrl: PopoverController
-  ){}
+  ) { }
 
-<<<<<<< HEAD
   presentPopover(myEvent) {
-      let popover = this.popoverCtrl.create(PopoverPage);
-      popover.present({
-        ev: myEvent
-      });
-    }
+    let popover = this.popoverCtrl.create(PopoverPage);
+    popover.present({
+      ev: myEvent
+    });
+  }
 
-  logIn(){
+  logIn() {
     this.navCtrl.push(PersonSignupPage);
-=======
-
->>>>>>> 4a6c3cc0184a9693b0cc19fd9b6e77b1e07fccbe
   }
 
 }
