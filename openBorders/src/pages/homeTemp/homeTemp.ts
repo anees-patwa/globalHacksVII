ba46414e1cd53140cac971f3517fb890c4bd37b6
Sam <skliu@wustl.edu>
2018-10-13 10:55:20 -0500

    no message

---
+++ openBorders/src/pages/homeTemp/homeTemp.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  selector: 'page-homeTemp',
+  templateUrl: 'homeTemp.html'
+})
+
+export class HomeTemp{
+  constructor(public navCtrl: NavController){
+    
+  }
+}
