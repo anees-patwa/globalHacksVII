a8faa65558f2b2b21acef9ead88e26cdf6da7fc5
apatwa17 <ahpandroid5@gmail.com>
2018-10-13 14:04:38 -0500

    added login by email and password for person

--- openBorders/src/pages/personlogin/personlogin.ts
+++ openBorders/src/pages/personlogin/personlogin.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import * as firebase from 'firebase';
 /**
  * Generated class for the PersonloginPage page.
  *
@@ -14,12 +14,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'personlogin.html',
 })
 export class PersonloginPage {
-
+  form = {};
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+
+  loginPerson() {
+    let email = this.form.email;
+    let password = this.form.password;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad PersonloginPage');
   }
 
 }
+
